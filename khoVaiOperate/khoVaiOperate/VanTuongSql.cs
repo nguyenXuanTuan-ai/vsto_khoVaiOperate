15466f3c9394a4c988206e25a4c317c39ee33f12
nguyenXuanTuan-ai <solfamiredoz@gmail.com>
2026-03-20 05:22:23 +0700

    commit

--- khoVaiOperate/khoVaiOperate/VanTuongSql.cs
+++ khoVaiOperate/khoVaiOperate/VanTuongSql.cs
@@ -8,145 +8,132 @@ namespace khoVaiOperate
 {
     public static class VanTuongSql
     {
-        public static string ipAddress = "192.168.1.168";
+        public static class congTySql
+        {
+            public static string ipAddress = "100.64.0.1";
+            public static class tableDanhSáchNhânViên
+            {
+                public static string name = "congTy.danhSáchNhânViên";
+                public static string họVàTên = "họVàTên";
+                public static string mậtKhẩu = "mậtKhẩu";
+                public static string lươngThờiGian = "lươngThờiGian";
+                public static string bậcThợMay = "bậcThợMay";
+                public static string xưởngMay = "xưởngMay";
+                public static string xưởngCắt = "xưởngCắt";
+                public static string khoVải = "khoVải";
+                public static string cánBộ = "cánBộ";
+                public static string cònLàm = "cònLàm";
+            }
+
+            public static class tableMãVạch
+            {
+                public static string name = "congTy.mãVạch";
+                public static string mãVạchThươngMại = "mãVạchThươngMại";
+                public static string mãVạchSảnXuất = "mãVạchSảnXuất";
+                public static string mãVạchQuốcGia = "mãVạchQuốcGia";
+                public static string tênHàng = "tênHàng";
+                public static string mãVạchPhổBiến = "mãVạchPhổBiến";
+            }
+            public static class tableDanhSáchKhuVực
+            {
+                public static string name = "congTy.danhSáchKhuVực";
+                public static string khuVực = "khuVực";
+            }
+        }
+        public static class KhoVaiSql
+        {
+            public static class tableCâyVải
+            {
+                public static string name = "khoVai.câyVải";
+                public static string thẻKho = "thẻKho";
+                public static string phiếuNhập = "phiếuNhập";
+                public static string sốĐơnHàngVải = "sốĐơnHàngVải";
+                public static string ngàyNhập = "ngàyNhập";
+                public static string nhàCungCấp = "nhàCungCấp";
+                public static string nhàCungCấpIn = "nhàCungCấpIn";
+                public static string mụcĐíchSửDụng = "mụcĐíchSửDụng";
+                public static string chấtLiệu = "chấtLiệu";
+                public static string sốQuyển = "sốQuyển";
+                public static string vịTrí = "vịTrí";
+                public static string khổVải = "khổVải";
+                public static string sốLượngNhập = "sốLượngNhập";
+                public static string đơnVị = "đơnVị";
+                public static string trọngLượng = "trọngLượng";
+                public static string sốMét = "sốMét";
+                public static string độBềnVảiVàMàu = "độBềnVảiVàMàu";
+                public static string độCoDàiPhầnTrăm = "độCoDàiPhầnTrăm";
+                public static string độCoRộngPhầnTrăm = "độCoRộngPhầnTrăm";
+                public static string chếĐộGiặt = "chếĐộGiặt";
+                public static string nguyênLiệu = "nguyênLiệu";
+                public static string địnhLượngg_m2 = "địnhLượngg_m2";
+                public static string thunSi = "thunSi";
+                public static string thẻKhoĐạiDiện = "thẻKhoĐạiDiện";
+                public static string ghiChú = "ghiChú";
+            }
+            public static class tableXuấtVải
+            {
+                public static string name = "khoVai.xuấtVải";
+                public static string ngày = "Ngày";
+                public static string phiếuXuấtVải = "phiếuXuấtVải";
+                public static string sốLượngXuất = "sốLượngXuất";
+                public static string thẻKho = "ThẻKho";
+            }
+            public static class tableĐơnVịNhậpCâyVải
+            {
+                public static string name = "khoVai.đơnVịNhậpCâyVải";
+                public static string đơnVị = "đơnVị";
+            }
+
+            public static class tableCâyVảiThunSi
+            {
+                public static string name = "khoVai.câyVảiThunSi";
+                public static string thunSi = "thunSi";
+            }
+            public static class tableVịTríKhoVải
+            {
+                public static string name = "khoVai.vịTríKhoVải";
+                public static string vịTrí = "vịTrí";
+                public static string khuVực = "khuVực";
+                public static string ghiChú = "ghiChú";
+            }
+
+        }
         public static class xuongMaySql
         {
+            public static class tableCôngĐoạnPhátSinhMay
+            {
+                public static string name = "xuongMay.côngĐoạnPhátSinhMay";
+                public static string tênCôngĐoạn = "tênCôngĐoạn";
+                public static string đơnGiá = "đơnGiá";
+                public static string đơnVị = "đơnVị";
+                public static string côngĐoạnĐóngBao = "côngĐoạnĐóngBao";
+                public static string côngĐoạnMay = "côngĐoạnMay";
+                public static string côngĐoạnPhổBiến = "côngĐoạnPhổBiến";
+                public static string côngĐoạnThờiGian = "côngĐoạnThờiGian";
+            }
+
             public static class tableLươngCôngViệc
             {
-                public static string name = "xuongMay.lươngCôngViệc";
-                public static string cộtNgày = "Ngày";
-
-                public static string cộtThẻNhập = "ThẻNhập";
-                public static string cộtTênCôngViệc = "TênCôngViệc";
-                public static string cộtGhiChú = "GhiChú";
-                public static string cộtKanbai = "Kanbai";
-                public static string cộtSốLượng = "SốLượng";
-                public static string cộtSốThứTự = "SốThứTự";
-                public static string cộtĐơnGiá = "ĐơnGiá";
-                public static string cộtThànhTiền = "ThànhTiền";
-                public static string cộtĐánhDấu = "đánhDấu";
-                public static List<string> danhSáchCột = new List<string>
-                { cộtNgày, cộtThẻNhập , cộtTênCôngViệc, cộtGhiChú, cộtKanbai, cộtSốLượng,
-                        cộtSốThứTự, cộtĐơnGiá, cộtThànhTiền, cộtĐánhDấu};
+                public static string name = "xuongmay.lươngCôngViệc";
+                public static string ngày = "Ngày";
+                public static string thẻNhập = "ThẻNhập";
+                public static string tênCôngViệc = "TênCôngViệc";
+                public static string ghiChú = "GhiChú";
+                public static string kanbai = "Kanbai";
+                public static string sốLượng = "sốLượng";
+                public static string sốThứTự = "sốThứTự";
+                public static string đơnGiá = "đơnGiá";
+                public static string thànhTiền = "thànhTiền";
+                public static string đánhDấu = "đánhDấu";
             }
             public static class tableLươngTheoNhóm
             {
                 public static string name = "xuongMay.lươngTheoNhóm";
-                public static string cộtNgày = "Ngày";
-                public static string cộtThẻNhập = "ThẻNhập";
-                public static string cộtTên = "Tên";
-                public static string cộtLươngTừngNgười = "lươngTừngNgười";
-                public static string cộtNgàyCông = "ngàyCông";
-                public static List<string> danhSáchCột = new List<string>
-                {
-                    cộtNgày, cộtThẻNhập, cộtTên, cộtLươngTừngNgười, cộtNgàyCông
-                };
-            }
-            public static class tableKanbai
-            {
-                public static string name = "xuongMay.kanbai";
-                public static string cộtKanbai = "sốKanbai";
-                public static string cộtSốLượng = "sốLượng";
-                public static string cộtSốPhiếuXuất = "sốPhiếuXuất";
-                public static List<string> danhSáchCột = new List<string>
-                {
-                    cộtKanbai, cộtSốLượng, cộtSốPhiếuXuất
-                };
-            }
-            public static class tablePhiếuXuấtBánThànhPhẩm
-            {
-                public static string name = "nhaCat.phiếuXuấtBtpXưởngMay";
-                public static string cộtSốPhiếuXuất = "sốPhiếuXuất";
-                public static string cộtMãĐơnGiá = "mãĐơnGiá";
-                public static string cộtTênHàng = "tênHàng";
-                public static string cộtSảnLượngCấp = "sảnLượngCấp";
-                public static List<string> danhSáchCột = new List<string>
-                {
-                    cộtSốPhiếuXuất, cộtMãĐơnGiá, cộtTênHàng, cộtSảnLượngCấp
-                };
-            }
-            public static class tableĐơnGiáMay
-            {
-                public static string name = "xuongMay.đơnGiáMay";
-                public static string cộtMãĐơnGiá = "mãĐơnGiá";
-                public static string cộtVòngTrong = "vòngTrong";
-                public static string cộtChuyênDùng = "chuyênDùng";
-                public static string cộtVòngNgoài = "vòngNgoài";
-                public static string cộtĐóngBao = "đóngBao";
-                public static string cộtỦi = "Ủi";
-                public static List<string> danhSáchCột = new List<string>
-                {
-                    cộtMãĐơnGiá, cộtVòngTrong, cộtChuyênDùng, cộtVòngNgoài, cộtĐóngBao, cộtỦi
-                };
-            }
-            public static class tableCôngĐoạnPhátSinhMay
-            {
-                public static string name = "xuongMay.côngĐoạnPhátSinhMay";
-                public static string cộtTênCôngĐoạn = "tênCôngĐoạn";
-                public static string cộtĐơnGiá = "đơnGiá";
-                public static string cộtĐơnVị = "đơnVị";
-                public static string cộtCôngĐoạnĐóngBao = "côngĐoạnĐóngBao";
-                public static string cộtCôngĐoạnMay = "côngĐoạnMay";
-                public static string cộtCôngĐoạnPhổBiến = "côngĐoạnPhổBiến";
-                public static string cộtCôngĐoạnThờiGian = "côngĐoạnThờiGian";
-                public static List<string> danhSáchCột = new List<string>
-                {
-                    cộtTênCôngĐoạn,  cộtĐơnGiá, cộtĐơnVị, cộtCôngĐoạnĐóngBao, cộtCôngĐoạnMay, cộtCôngĐoạnPhổBiến, cộtCôngĐoạnThờiGian
-                };
-            }
-            public static class tableLươngThờiGian
-            {
-                public static string name = "xuongMay.lươngThờiGian";
-                public static string cộtTênCôngViệc = "tênCôngViệc";
-                public static string cộtĐơnVị = "đơnVị";
-                public static List<string> danhSáchCột = new List<string>
-                {
-                    cộtTênCôngViệc, cộtĐơnVị
-                };
-            }
-            public static class tableCôngĐoạnMay
-            {
-                public static string name = "xuongMay.côngĐoạnMay";
-                public static string cộtTênCôngĐoạn = "tênCôngĐoạn";
-                public static string cộtĐơnVị = "đơnVị";
-                public static List<string> danhSáchCột = new List<string>
-                {
-                    cộtTênCôngĐoạn, cộtĐơnVị
-                };
-            }
-            public static class tableBậcThợMay
-            {
-                public static string name = "xuongMay.bậcThợMay";
-                public static string cộtBậcThợ = "bậcThợ";
-                public static string cộtHệSốBậcThợ = "hệSốBậcThợ";
-            }
-        }
-        public static class congTySql
-        {
-            public static class tableDanhSáchNhânViên
-            {
-                public static string name = "congTy.danhSáchNhânViên";
-                public static string cộtLươngThờiGian = "lươngThờiGian";
-                public static string cộtHọVàTên = "họVàTên";
-                public static string cộtBậcThợMay = "bậcThợMay";
-                public static string cộtHệSốMay = "hệSốMay";
-                public static string cộtXưởngMay = "xưởngMay";
-                public static string cộtCònLàm = "cònLàm";
-            }
-        }
-        public static class nhaCatSql
-        {
-            public static class tablePhiếuXuấtBtp
-            {
-                public static string name = "nhaCat.phiếuXuấtBtpXưởngMay";
-                public static string sốPhiếuXuất = "sốPhiếuXuất";
-                public static string mãĐơnGiá = "mãĐơnGiá";
-                public static string tênHàng = "tênHàng";
-                public static string sảnLượngCấp = "sảnLượngCấp";
-                public static List<string> danhSáchCột = new List<string>
-                {
-                    sốPhiếuXuất, mãĐơnGiá, tênHàng, sảnLượngCấp
-                };
+                public static string ngày = "Ngày";
+                public static string thẻNhập = "ThẻNhập";
+                public static string tên = "Tên";
+                public static string lươngTừngNgười = "lươngTừngNgười";
+                public static string ngàyCông = "ngàyCông";
             }
         }
     }
